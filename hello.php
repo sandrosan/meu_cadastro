787931a1c4932432576e91f2c054e4c2d2d17d31
Sandro <sandrosan75@hotmail.com>
2016-06-04 12:11:52 -0300

    formulario criado

--- hello.php
+++ hello.php
@@ -6,5 +6,16 @@
 	<h1>Sejam Bem-Vindo</h1>
 	<h2>informe seus dados</h2>
 
+
+	<form method="POST" action=""> 
+	<label> nome:</label>
+	<input name="nome_usuario" type="text">
+	<label>email:</label>
+	<input email="emaei_do_usuario" type="email" >
+	<input nome="salvar" value="confirmar" type="submit">
+	<input nome="salvar" value="limpar" type="reset">
+
+	</form>
+
 </body>
 </html>
