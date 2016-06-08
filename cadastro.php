b1b308b40573d333c0a91fe106139dd3773cc0ba
sandro <sandrosan75@hotmail.com>
2016-06-08 17:22:26 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -11,11 +11,11 @@
 	<label> nome:</label>
 	<input name="nome_usuario" type="text">
 	<label>email:</label>
-	<input E-mail="email_usuario" type="email" >
+	<input name="email_usuario" type="email" >
 	<label>senha:</label>
-	<input nome="senha_usuario" type="password"
-	<input nome="salvar" value="confirmar" type="submit">
-	<input nome="salvar" value="limpar" type="reset">
+	<input name="senha_usuario" type="password"
+	<input name="salvar" value="confirmar" type="submit">
+	<input name="salvar" value="limpar" type="reset">
 
 	</form>
 
