18c113782695c63d00ecd69ed504a5ef6696fbb8
sandrosan <sandrosan75@hotmail.com>
2016-06-08 16:35:53 -0300

    Update and rename hello.php to cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,17 +1,17 @@
 <html>
 <head>
-	<title>Pgina Inicial</title>
+	<title>Pagina Inicial</title>
 </head>
 <body>
 	<h1>Sejam Bem-Vindo</h1>
 	<h2>informe seus dados</h2>
 
 
-	<form method="POST" action=""> 
+	<form method="POST" action="resposta.php"> 
 	<label> nome:</label>
 	<input name="nome_usuario" type="text">
 	<label>email:</label>
-	<input email="email_usuario" type="email" >
+	<input E-mail="email_usuario" type="email" >
 	<label>senha:</label>
 	<input nome="senha_usuario" type="password"
 	<input nome="salvar" value="confirmar" type="submit">
